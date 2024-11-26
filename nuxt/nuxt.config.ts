ba1ec17b7bd0f02fde63e3130f28ddab3055974f
edgeidr <edge.idr@gmail.com>
2024-11-26 12:32:57 +0800

    primvue + tailwind + login page

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,5 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+	compatibilityDate: "2024-11-01",
+	devtools: { enabled: true },
+	ssr: false,
+	app: {
+		head: {
+			title: process.env.NUXT_APP_NAME || "",
+		},
+	},
+	runtimeConfig: {
+		public: {
+			appName: process.env.NUXT_APP_NAME || "",
+		},
+	},
+	modules: ["@primevue/nuxt-module", "@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+	primevue: {
+		importTheme: { from: "@/themes/mytheme.js" },
+		options: {
+			ripple: true,
+			inputVariant: "filled",
+		},
+	},
+	css: ["primeicons/primeicons.css", "@/assets/css/base.css"],
+	tailwindcss: {
+		configPath: "tailwind.config.js",
+	},
+	googleFonts: {
+		families: {
+			Poppins: [100, 300, 400, 500, 700, 900],
+			Montserrat: [100, 300, 400, 500, 700, 900],
+		},
+	},
+});
---
+++ nuxt/pages/login/index.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+		<div class="flex flex-col space-y-2 text-center">
+			<h1 class="text-2xl font-semibold tracking-tight">Welcome back!</h1>
+			<p class="text-sm text-muted-foreground">Enter your email below to sign in</p>
+		</div>
+
+		<div class="grid gap-6">
+			<form @submit.prevent="onSubmit">
+				<div class="grid gap-2">
+					<div class="grid gap-1">
+						<label class="sr-only" for="email"> Email </label>
+						<InputText placeholder="name@example.com" type="email" />
+					</div>
+					<Button label="Sign In with Email" :icon="isLoading ? 'pi pi-spinner pi-spin' : ''" :loading="isLoading" />
+				</div>
+			</form>
+			<div class="relative">
+				<div class="absolute inset-0 flex items-center">
+					<span class="w-full border-t" />
+				</div>
+				<div class="relative flex justify-center text-xs">
+					<span class="bg-surface-0 px-2 text-muted-foreground"> or continue with </span>
+				</div>
+			</div>
+			<Button
+				variant="outline"
+				severity="secondary"
+				label="Google"
+				:icon="isLoadingGoogle ? 'pi pi-spinner pi-spin' : 'pi pi-google'"
+				:loading="isLoading" />
+		</div>
+
+		<p class="px-8 text-center text-sm text-muted-foreground">
+			By clicking continue, you agree to our
+			<NuxtLink to="/terms" class="underline underline-offset-4 hover:text-primary"> Terms of Service </NuxtLink>
+			and
+			<NuxtLink to="/privacy" class="underline underline-offset-4 hover:text-primary"> Privacy Policy </NuxtLink>
+			.
+		</p>
+	</div>
+</template>
+
+<script setup lang="ts">
+	definePageMeta({
+		layout: "authentication",
+	});
+
+	const isLoading = ref(false);
+	const isLoadingGoogle = ref(false);
+
+	const onSubmit = () => {
+		isLoading.value = true;
+
+		setTimeout(async () => {
+			isLoading.value = false;
+			await navigateTo({ name: "index" });
+		}, 3000);
+	};
+</script>
