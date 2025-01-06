27027970afa72e5c98ae3de91ff6cbdd3e3c3806
edgeidr <edge.idr@gmail.com>
2025-01-06 20:34:26 +0800

    sample times for timegrid

--- nuxt/components/shared/timegrid.vue
+++ nuxt/components/shared/timegrid.vue
@@ -1,66 +1,72 @@
 <template>
-    <div class="flex-1">
-        <FullCalendar ref="calendarRef" :options="calendarOptions" />
-    </div>
+	<div class="flex-1">
+		<FullCalendar ref="calendarRef" :options="calendarOptions" />
+	</div>
 </template>
 
 <script setup lang="ts">
-import FullCalendar from "@fullcalendar/vue3";
-import interactionPlugin from "@fullcalendar/interaction";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import type { CalendarApi, CalendarOptions } from "@fullcalendar/core";
+	import FullCalendar from "@fullcalendar/vue3";
+	import interactionPlugin from "@fullcalendar/interaction";
+	import timeGridPlugin from "@fullcalendar/timegrid";
+	import type { CalendarApi, CalendarOptions } from "@fullcalendar/core";
 
-const calendarRef = ref();
-const headerHeight = useState<number>("headerHeight");
-const selectedDate = useState<Date>("selectedDate");
-const topSpacing = computed(() => headerHeight.value);
-const calendarApi = computed<CalendarApi>(() => calendarRef.value?.getApi());
+	const calendarRef = ref();
+	const headerHeight = useState<number>("headerHeight");
+	const selectedDate = useState<Date>("selectedDate");
+	const topSpacing = computed(() => headerHeight.value);
+	const calendarApi = computed<CalendarApi>(() => calendarRef.value?.getApi());
 
-useState("calendarApi", () => calendarApi);
+	useState("calendarApi", () => calendarApi);
 
-const calendarOptions = computed<CalendarOptions>(() => {
-    return {
-        headerToolbar: false,
-        plugins: [interactionPlugin, timeGridPlugin],
-        initialView: "timeGrid",
-        nowIndicator: true,
-        editable: true,
-        allDaySlot: false,
-        scrollTimeReset: false,
-        eventTimeFormat: {
-            hour: "2-digit",
-            minute: "2-digit",
-            meridiem: "lowercase",
-        },
-        slotLabelFormat: {
-            hour: "2-digit",
-            minute: "2-digit",
-            meridiem: "lowercase",
-        },
+	const calendarOptions = computed<CalendarOptions>(() => {
+		return {
+			headerToolbar: false,
+			plugins: [interactionPlugin, timeGridPlugin],
+			initialView: "timeGrid",
+			nowIndicator: true,
+			editable: true,
+			allDaySlot: false,
+			scrollTimeReset: false,
+			eventTimeFormat: {
+				hour: "2-digit",
+				minute: "2-digit",
+				meridiem: "lowercase",
+			},
+			slotLabelFormat: {
+				hour: "2-digit",
+				minute: "2-digit",
+				meridiem: "lowercase",
+			},
 
-        slotDuration: "00:15",
-        slotLabelInterval: "01:00",
-        height: "auto",
-        dayHeaderFormat: { weekday: "short", day: "numeric" },
-        initialEvents: [{ title: "nice event", start: new Date() }],
-    };
-});
+			slotDuration: "00:15",
+			slotLabelInterval: "01:00",
+			height: "auto",
+			dayHeaderFormat: { weekday: "short", day: "numeric" },
+			initialEvents: [
+				{ title: "First event", start: new Date().setHours(6, 0, 0) },
+				{ title: "Second event", start: new Date().setHours(6, 45, 0) },
+				{ title: "Third event", start: new Date().setHours(8, 0, 0), end: new Date().setHours(10, 0, 0) },
+				{ title: "Fourth event", start: new Date().setHours(15, 0, 0), end: new Date().setHours(18, 0, 0) },
+				{ title: "Fifth event", start: new Date().setHours(20, 0, 0), end: new Date().setHours(22, 30, 0) },
+			],
+		};
+	});
 
-watch(
-    () => topSpacing.value,
-    (newValue) => {
-        const FC_HEADERS = <NodeListOf<HTMLElement>>document?.querySelectorAll(".fc .fc-scrollgrid-section-sticky > *");
+	watch(
+		() => topSpacing.value,
+		(newValue) => {
+			const FC_HEADERS = <NodeListOf<HTMLElement>>document?.querySelectorAll(".fc .fc-scrollgrid-section-sticky > *");
 
-        for (const FC_HEADER of FC_HEADERS) {
-            FC_HEADER.style.top = `${newValue}px`;
-        }
-    },
-);
+			for (const FC_HEADER of FC_HEADERS) {
+				FC_HEADER.style.top = `${newValue}px`;
+			}
+		},
+	);
 
-watch(
-    () => selectedDate.value,
-    (newValue) => {
-        calendarApi.value?.gotoDate(newValue);
-    },
-);
+	watch(
+		() => selectedDate.value,
+		(newValue) => {
+			calendarApi.value?.gotoDate(newValue);
+		},
+	);
 </script>
