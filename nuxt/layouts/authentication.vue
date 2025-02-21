f621f75eeb7f235387a2a9ccd08658bccf894010
edgeidr <edge.idr@gmail.com>
2025-02-21 13:38:50 +0800

    fix Auth layout

--- nuxt/layouts/authentication.vue
+++ nuxt/layouts/authentication.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<div class="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-			<div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+			<div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
 				<div class="absolute inset-0 bg-zinc-900" />
 				<div class="relative z-20 flex items-center text-lg font-medium">
 					<svg
@@ -19,13 +19,11 @@
 				</div>
 				<div class="relative z-20 mt-auto">
 					<blockquote class="space-y-2">
-						<footer class="text-sm">
-							If you are unable to remember your password, please reach out to the administrator for assistance.
-						</footer>
+						<footer class="text-sm">If you are unable to remember your password, please reach out to the administrator for assistance.</footer>
 					</blockquote>
 				</div>
 			</div>
-			<div class="lg:p-8 h-screen place-content-center">
+			<div class="flex h-screen items-center justify-center lg:p-8">
 				<slot />
 			</div>
 		</div>
